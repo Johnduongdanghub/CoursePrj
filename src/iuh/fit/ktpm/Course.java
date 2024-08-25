1d9a35fb35787614d2ef69e8742299f4b3adfea2
origin <23661191.duong@student.iuh.edu.vn>
2024-08-25 18:53:59 +0700

    CoursePrj

---
+++ src/iuh/fit/ktpm/Course.java
@@ -0,0 +1,79 @@
+package iuh.fit.ktpm;
+
+/**
+ * @description: This class represent a rectangle
+ * @author: Dương, Đặng Trần Dương
+ * @version: 1.0
+ * @created: 25/08/2024 6:44 CH
+ */
+public class Course {
+    //Attributes
+    private String id;
+    private String title;
+    private String department;
+    private int credit;
+
+    //Constructors
+    public Course() {
+        this("", "", "", 0);
+    }
+    public Course(String id, String title, String department, int credit) {
+        this.id = id;
+        this.title = title;
+        this.department = department;
+        this.credit = credit;
+    }
+
+    //Properties set/get
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        if(id.length() < 3){
+            throw new IllegalArgumentException("ID must be at least 3 characters");
+        }
+        if(!id.matches("[a-zA-Z0-9]+")) {
+            throw new IllegalArgumentException("ID must contain only letters or digits");
+        }
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        if(title == null || title.isEmpty()){
+            throw new IllegalArgumentException("Title must not be empty");
+        }
+        this.title = title;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getCredit() {
+        return credit;
+    }
+
+    public void setCredit(int credit) {
+        if(credit < 0){
+            throw new IllegalArgumentException("Credit must be greater than or equal to 0");
+        }
+        this.credit = credit;
+    }
+    @Override
+    public String toString() {
+        return "Course:  " +
+                "id ='" + id + '\'' +
+                ", title ='" + title + '\'' +
+                ", credit =" + credit +
+                ", department ='" + department + '\'';
+    }
+}
